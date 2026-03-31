4ee5f7d81c4a7cd25a8816fe7241af62662ea882
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-03-31 19:26:53 +0700

    menambahkan case 1

--- switchCase.cpp
+++ switchCase.cpp
@@ -16,3 +16,19 @@ void input(){
     cout << "Masukkan kode jurusan (1-3): ";
     cin >> kodeJurusan;
 }
+
+// prosedur untuk pngecekan kondisi
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
+        case 2:
+            namaJurusan = "Teknologi Elektro";
+            statusKuota = "Kuota Penuh"
+            break;
+        
+    }
+    }
+}
